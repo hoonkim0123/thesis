7b786519fae2f5d254bdac2953a0c7a64731d9b7
Saehun Kim <saehunk9561@gmail.com>
2026-04-09 17:07:48 +0000

    git fetch update

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -4,9 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const githubRepo = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: githubRepo ? `/${githubRepo}/` : '/',
   plugins: [
     vue(),
     vueDevTools(),
