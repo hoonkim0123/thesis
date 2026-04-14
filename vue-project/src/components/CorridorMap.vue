a2b6097e1c0ef2106002493d3405c3ee3185c302
Saehun Kim <saehunk9561@gmail.com>
2026-04-14 18:03:35 +0000

    zoom up the map

--- vue-project/src/components/CorridorMap.vue
+++ vue-project/src/components/CorridorMap.vue
@@ -87,7 +87,7 @@ onMounted(async () => {
 
   map = L.map(mapEl.value, {
     center: [40.755, -73.985],
-    zoom: 12,
+    zoom: 18,
     zoomControl: false,
     attributionControl: false,
     scrollWheelZoom: false,
