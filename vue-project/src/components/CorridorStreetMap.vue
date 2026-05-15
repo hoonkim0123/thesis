6c22abc67c3044d534727c77ebaa8997c622c9ac
Saehun Kim <hoonkim0123@users.noreply.github.com>
2026-05-15 00:06:55 -0400

    Fix count for 2nd Avenue corridor from 19 to 18 in CorridorStreetMap.vue

--- vue-project/src/components/CorridorStreetMap.vue
+++ vue-project/src/components/CorridorStreetMap.vue
@@ -6,7 +6,7 @@ const emit = defineEmits(['streetSelected'])
 const STREETS = [
   { name: 'Amsterdam Avenue', area: 'Upper West Side', corridor_label_clean: 'Amsterdam Ave (Upper West Side)', n: 19 },
   { name: 'Columbus Avenue', area: 'Upper West Side', corridor_label_clean: 'Columbus Ave (Upper West Side)', n: 13 },
-  { name: '2nd Avenue', area: 'Upper East Side', corridor_label_clean: '2nd Ave (Upper East Side)', n: 19 },
+  { name: '2nd Avenue', area: 'Upper East Side', corridor_label_clean: '2nd Ave (Upper East Side)', n: 18 },
   { name: 'Mulberry Street', area: 'Little Italy / SoHo', corridor_label_clean: 'Mulberry St (Little Italy / SoHo)', n: 13 },
 ]
 
